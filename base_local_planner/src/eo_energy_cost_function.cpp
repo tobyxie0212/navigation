4bf05593b1f46970182747dffd15261ff9124f3d
tobyxie0212 _ on student Laptop <tobyxie0212@163.com>
2016-04-18 20:25:55 +1200

    straight experiment update

--- base_local_planner/src/eo_energy_cost_function.cpp
+++ base_local_planner/src/eo_energy_cost_function.cpp
@@ -331,13 +331,13 @@ if (n > 1) {
 	I_motor_pred[3] = ( ( cos(rot)-sin(rot))*0.3535*copysign(1.0,wheel_rot_vel[3])*m_auckbot*acc_mean[0] + ( sin(rot)+cos(rot))*0.3535*copysign(1.0,wheel_rot_vel[3])*m_auckbot*acc_mean[1] - 0.54*copysign(1.0,wheel_rot_vel[3])*I_auckbot*acc_mean[2] +u_viscous_fric*0.25*m_auckbot*G*wheel_vel_p[3] + u_static_fric[3]*0.25*m_auckbot*G*copysign(1.0,wheel_vel_p[3]) )/36.25;
 	I_motor_pred[4] = ( (-cos(rot)-sin(rot))*0.3535*copysign(1.0,wheel_rot_vel[4])*m_auckbot*acc_mean[0] + (-sin(rot)+cos(rot))*0.3535*copysign(1.0,wheel_rot_vel[4])*m_auckbot*acc_mean[1] - 0.54*copysign(1.0,wheel_rot_vel[4])*I_auckbot*acc_mean[2] +u_viscous_fric*0.25*m_auckbot*G*wheel_vel_p[4] + u_static_fric[4]*0.25*m_auckbot*G*copysign(1.0,wheel_vel_p[4]) )/36.25;
 
-	if ( ( (vel_mean[0]==0)&&(vel_mean[1]==0) )||( (vel_mean[1]==0)&&(vel_mean[2]==0) )||( (vel_mean[0]==0)&&(vel_mean[2]==0) ) ) {
+	//if ( ( (vel_mean[0]==0)&&(vel_mean[1]==0) )||( (vel_mean[1]==0)&&(vel_mean[2]==0) )||( (vel_mean[0]==0)&&(vel_mean[2]==0) ) ) {
 	//primitive motion model requires current term to be scaled down by four
-	I_motor_pred[1] = I_motor_pred[1]/4;
-	I_motor_pred[2] = I_motor_pred[2]/4;
-	I_motor_pred[3] = I_motor_pred[3]/4;
-	I_motor_pred[4] = I_motor_pred[4]/4;
-	} 
+	//I_motor_pred[1] = I_motor_pred[1]/4;
+	//I_motor_pred[2] = I_motor_pred[2]/4;
+	//I_motor_pred[3] = I_motor_pred[3]/4;
+	//I_motor_pred[4] = I_motor_pred[4]/4;
+	//} 
 
 	P_traj_elec = R_armature * I_motor_pred[1] * I_motor_pred[1] + R_armature * I_motor_pred[2] * I_motor_pred[2] + R_armature * I_motor_pred[3] * I_motor_pred[3] + R_armature * I_motor_pred[4] * I_motor_pred[4];
 
@@ -345,7 +345,7 @@ if (n > 1) {
 	P_traj_mech = M_torque_fric * wheel_rot_vel[1] * wheel_rot_vel[1] + M_torque_fric * wheel_rot_vel[2] * wheel_rot_vel[2] + M_torque_fric * wheel_rot_vel[3] * wheel_rot_vel[3] + M_torque_fric * wheel_rot_vel[4] * wheel_rot_vel[4];
 
 	//idle consumption - onboard devices consumption
-	P_traj_idle = 76.32;	//standstill current 1.59 A
+	P_traj_idle = 76.32;	//standstill current 1.59 A* 48 V
 
 	E_traj = (P_traj_kine + P_traj_fric + P_traj_elec + P_traj_mech + P_traj_idle) * n * t;
 
@@ -435,13 +435,13 @@ if (n > 1) {
 		I_motor_pred_end[3] = ( u_viscous_fric*0.25*m_auckbot*G*wheel_vel_p_end[3] + u_static_fric_end[3]*0.25*m_auckbot*G*copysign(1.0,wheel_vel_p_end[3]) )/36.25;
 		I_motor_pred_end[4] = ( u_viscous_fric*0.25*m_auckbot*G*wheel_vel_p_end[4] + u_static_fric_end[4]*0.25*m_auckbot*G*copysign(1.0,wheel_vel_p_end[4]) )/36.25;
 
-		if ( ( (vel_end[0]==0)&&(vel_end[1]==0) )||( (vel_end[1]==0)&&(vel_end[2]==0) )||( (vel_end[0]==0)&&(vel_end[2]==0) ) ) {
+		//if ( ( (vel_end[0]==0)&&(vel_end[1]==0) )||( (vel_end[1]==0)&&(vel_end[2]==0) )||( (vel_end[0]==0)&&(vel_end[2]==0) ) ) {
 		//primitive motion model requires current term scale down
-		I_motor_pred_end[1] = I_motor_pred_end[1]/4;
-		I_motor_pred_end[2] = I_motor_pred_end[2]/4;
-		I_motor_pred_end[3] = I_motor_pred_end[3]/4;
-		I_motor_pred_end[4] = I_motor_pred_end[4]/4;
-		} 
+		//I_motor_pred_end[1] = I_motor_pred_end[1]/4;
+		//I_motor_pred_end[2] = I_motor_pred_end[2]/4;
+		//I_motor_pred_end[3] = I_motor_pred_end[3]/4;
+		//I_motor_pred_end[4] = I_motor_pred_end[4]/4;
+		//} 
 
 		P_traj_elec_end = R_armature * I_motor_pred_end[1] * I_motor_pred_end[1] + R_armature * I_motor_pred_end[2] * I_motor_pred_end[2] + R_armature * I_motor_pred_end[3] * I_motor_pred_end[3] + R_armature * I_motor_pred_end[4] * I_motor_pred_end[4];
 
@@ -458,7 +458,7 @@ if (n > 1) {
 	//traj_scale += 0.3;
 	if (traj_scale > 1) traj_scale = 1;
 	//TODO: self_scale used to scale the energy term in the DWA cost functiion
-	self_scale = 0.7;
+	self_scale = 0.5;
 
   // ROS_INFO(">>> scoreTrajectory s:%d, l:%.2f, r:%.2f, v:%.3f, a:%.3f, e:%.3f", \
   //   traj.getPointsSize(), traj_length, rot, vel[0][0], acc[0][0], E_traj);
